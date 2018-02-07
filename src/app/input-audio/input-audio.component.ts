4e5b72d58184583aed6c0a26b42cd563a769da3a
Brian Margolis <brian.m.margolis@gmail.com>
2018-02-07 00:44:18 -0600

    Display a spectrogram with audio

--- src/app/input-audio/input-audio.component.ts
+++ src/app/input-audio/input-audio.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {  } from '../../../lib/colormap.min.js'
 
 @Component({
   selector: 'app-input-audio',
@@ -11,6 +12,9 @@ export class InputAudioComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // TODO: make this work (https://www.npmjs.com/package/color-map)?
+    var spectrogramColorMap = colormap();
+
     var height = 256;
     this.ws = WaveSurfer.create({
       container: '.input_audio',
@@ -18,16 +22,20 @@ export class InputAudioComponent implements OnInit {
       progressColor: '#FF00FF',
       // For the spectrogram the height is half the number of fftSamples
       fftSamples: height * 2,
-      height: height
+      height: height,
+      // colorMap: spectrogramColorMap
+      // visualization: "spectrogram"
     });
-
+    this.ws.params.visualization = "spectrogram"
+    this.ws.params.feedback = "none"
+    
     this.ws.load('https://ia902606.us.archive.org/35/items/shortpoetry_047_librivox/song_cjrg_teasdale_64kb.mp3')
 
     this.ws.enableDragSelection();
   }
 
-  pause() {
-    console.log("a")
+  playPause() {
+    this.ws.playPause()
   }
 
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,6 +1,7 @@
 /* SystemJS module definition */
 declare var module: NodeModule;
 declare var WaveSurfer:any;
+declare var colormap:any;
 interface NodeModule {
   id: string;
 }

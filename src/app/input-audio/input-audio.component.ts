b1eed6d242e2fb09692ee4435d9614610eac47cd
Brian Margolis <brian.m.margolis@gmail.com>
2018-02-07 08:29:05 -0600

    Make region colors constants

--- src/app/input-audio/input-audio.component.ts
+++ src/app/input-audio/input-audio.component.ts
@@ -54,6 +54,9 @@ export class InputAudioComponent implements OnInit {
   ws = null;
   selectedRegion = null;
 
+  SELECTED_COLOR = 'rgba(0, 0, 0, .6)'
+  UNSELECTED_COLOR = 'rgba(0, 0, 0, .3)'
+
   constructor() { }
 
   ngOnInit() {
@@ -110,8 +113,8 @@ export class InputAudioComponent implements OnInit {
     // to the API
     // So, we use JQuery.
     var region_name = region.id;
-    $("region[data-id='" + region_name + "']").css('backgroundColor', 'rgba(0, 0, 0, .6)');
-    $("region[data-id!='" + region_name + "']").css('backgroundColor', 'rgba(0, 0, 0, .3)');
+    $("region[data-id='" + region_name + "']").css('backgroundColor', this.SELECTED_COLOR);
+    $("region[data-id!='" + region_name + "']").css('backgroundColor', this.UNSELECTED_COLOR);
   }
 
   // Playback

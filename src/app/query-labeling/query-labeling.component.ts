8eccd2909a9593f717114ab4402317e1b7ea08c6
Brian Margolis <brian.m.margolis@gmail.com>
2018-02-24 03:51:52 -0600

    Bug fix - copy the global regions array rather than modify it in a child component

--- src/app/query-labeling/query-labeling.component.ts
+++ src/app/query-labeling/query-labeling.component.ts
@@ -15,16 +15,16 @@ export class QueryLabelingComponent implements OnInit {
     }
     // We want to modify the regions, not outright replace them. 
     // This is important because angular would otherwise recreate the DOM elements for each region every time they update, wiping out the text box. 
-
+    var updated_regions = regions.slice();
     // Go through the existing regions and update them or remove them
     for (var i = 0; i < this._regions.length; i++) {
       var region = this._regions[i];
 
-      var updated_region_index = regions.findIndex(updated_region => region.id == updated_region.id);
-      var updated_region = regions[updated_region_index]
+      var updated_region_index = updated_regions.findIndex(updated_region => region.id == updated_region.id);
+      var updated_region = updated_regions[updated_region_index]
 
       if (updated_region) {
-        regions.splice(updated_region_index, 1)
+        updated_regions.splice(updated_region_index, 1)
         region.start = updated_region.start;
         region.end = updated_region.end;
         region.label = updated_region.label;
@@ -35,8 +35,7 @@ export class QueryLabelingComponent implements OnInit {
     }
 
     // Add new regions
-    for (var updated_region of regions) {
-      console.log("adding: " + updated_region.id)
+    for (var updated_region of updated_regions) {
       this._regions.push(updated_region);
     }
   }
@@ -50,12 +49,9 @@ export class QueryLabelingComponent implements OnInit {
   @Output() onSelectRegion = new EventEmitter<string>();
   @Output() onSubmit = new EventEmitter<any>();
 
-  constructor() {
+  constructor() { }
 
-  }
-
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
   onKey(id: string, value: string) {
     this.updateLabel(id, value);
@@ -71,6 +67,7 @@ export class QueryLabelingComponent implements OnInit {
     })[0];
 
     region.label = value;
+    console.log(region);
     this.onUpdateLabel.emit(region);
   }
 
